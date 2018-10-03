882316edaaac6b0f761c8e3feb5479bc88042119
joshua1988 <jangkeehyo@gmail.com>
2018-10-03 13:36:58 +0900

    Removed the lines for practice

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 var path = require('path');
-var SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
 
 module.exports = {
   mode: 'none',
@@ -14,16 +13,5 @@ module.exports = {
     publicPath: '/dist/'
   },
 
-  plugins: [
-    new SWPrecacheWebpackPlugin({
-      cacheId: 'pwa-offline-v5',
-      filename: 'service-worker.js',
-      staticFileGlobs: [
-        'index.html',
-        'css/*.css',
-        'images/**.*',
-      ],
-      mergeStaticsConfig: true, // 웹팩 빌드 결과 파일들을 서비스 워커 캐싱 목록에 추가
-    }),
-  ],
+    
 }
